104667f121becbd49abe67e024b23d0a7c7c66c2
Wesley Cristiano <wsley@hotmail.com>
2020-06-21 17:55:41 -0400

    Challenge concept of React Navite on Bootcamp gostack

--- src/App.js
+++ src/App.js
@@ -22,9 +22,7 @@ export default function App() {
   },[])
 
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
     const response = await api.post(`repositories/${id}/like`)
- //const reps = 
     setRepositories(repositories.map(repository=> 
               repository.id===id? response.data: repository))
     }
@@ -48,50 +46,16 @@ export default function App() {
                 <View style={styles.likesContainer}>
                   <Text
                       style={styles.likeText}
-                      // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
                   testID={`repository-likes-${repository.id}`}>{repository.likes>1? `${repository.likes} curtidas`:`${repository.likes} curtida`}</Text>
                 </View>
                 <TouchableOpacity
                   style={styles.button}
                   onPress={() => handleLikeRepository(repository.id)}
-                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
                   testID={`like-button-${repository.id}`}>
                   <Text style={styles.buttonText}>Curtir</Text>
                 </TouchableOpacity>
               </View> 
             )}/>
-          
-        {/* <View style={styles.repositoryContainer}>
-                          <Text style={styles.repository}>Repository 1</Text>
-
-                          <View style={styles.techsContainer}>
-                            <Text style={styles.tech}>
-                              ReactJS
-                            </Text>
-                            <Text style={styles.tech}>
-                              Node.js
-                            </Text>
-                          </View>
-
-                          <View style={styles.likesContainer}>
-                            <Text
-                              style={styles.likeText}
-                              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-                              testID={`repository-likes-1`}
-                            >
-                              3 curtidas
-                            </Text>
-                          </View>
-
-                        <TouchableOpacity
-                          style={styles.button}
-                          onPress={() => handleLikeRepository(1)}
-                          // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                          testID={`like-button-1`}
-                        >
-                          <Text style={styles.buttonText}>Curtir</Text>
-                        </TouchableOpacity>
-                      </View>  */}
       </SafeAreaView>
     </>
   );
